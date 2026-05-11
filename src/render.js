60fd8bb84f3235da8f7ae3e2578a32d8191beb1d
B1ZON-c0de <roman.andreicko@icloud.com>
2026-05-11 23:06:30 +0700

    docs(render.js): добавил комментарии

--- src/render.js
+++ src/render.js
@@ -1,11 +1,14 @@
 export function renderSwitchButtons(quantity, parent) {
+  // Создает контейнер для кнопок
   const switchContainer = document.createElement("div")
 
+  // Добавляет и создает переданное кол-во кнопок
   for (let i = 0; i < quantity; i++) {
     const btn = document.createElement("button")
     btn.textContent = String(i)
     switchContainer.appendChild(btn)
   }
 
+  // Добавляет контейнер с кнопками в в #app
   parent.appendChild(switchContainer);
 }
