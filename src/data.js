f182350fc0738a05b1e1a15fd8c16733d5a1d58b
B1ZON-c0de <roman.andreicko@icloud.com>
2026-05-12 13:42:19 +0700

    feat(data.js): добавил svg картинки для каждого фона

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 import rainyBg from "./assets/rainy-bg.jpg"
 import summerBg from "./assets/summer-bg.jpg"
-import winterBg from"./assets/winter-bg.jpg"
+import winterBg from "./assets/winter-bg.jpg"
+import rainySvg from "./assets/icons/cloud-rain.svg"
+import winterSvg from "./assets/icons/cloud-snow.svg"
+import summerSvg from "./assets/icons/sun.svg"
 
 export const BACKGROUNDS = [
-  {id: 1, class: "rainy-button",url: rainyBg},
-  {id: 2, class: "summer-button",url: summerBg},
-  {id: 3, class: "winter-button", url: winterBg},
+  { id: 1, class: "rainy-button", url: rainyBg, svg: rainySvg },
+  { id: 2, class: "summer-button", url: summerBg, svg: summerSvg },
+  { id: 3, class: "winter-button", url: winterBg, svg: winterSvg },
 ]
