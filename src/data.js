36e5293cba9bc7eadfc96019e2e36800ce64f8ae
B1ZON-c0de <roman.andreicko@icloud.com>
2026-05-12 13:03:13 +0700

    fix(data.js): сделал импорт фонов в js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
+import rainyBg from "./assets/rainy-bg.jpg"
+import summerBg from "./assets/summer-bg.jpg"
+import winterBg from"./assets/winter-bg.jpg"
+
 export const BACKGROUNDS = [
-  {id: 1, class: "rainy-button",url: "/assets/rainy-bg.jpg"},
-  {id: 2, class: "summer-button",url: "/assets/summer-bg.jpg"},
-  {id: 3, class: "winter-button", url: "/assets/winter-bg.jpg"},
+  {id: 1, class: "rainy-button",url: rainyBg},
+  {id: 2, class: "summer-button",url: summerBg},
+  {id: 3, class: "winter-button", url: winterBg},
 ]
