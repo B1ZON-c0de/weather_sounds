bb5aff628a9c499befeac844fce0172d9445fd87
B1ZON-c0de <roman.andreicko@icloud.com>
2026-05-13 17:15:22 +0700

    refactor(shared.ts): добавил типизацию

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,4 @@
-export function createSVG(textSVG) {
+export function createSVG(textSVG: string): HTMLElement{
   const parser = new DOMParser()
   return parser.parseFromString(textSVG, "image/svg+xml").documentElement
 }
